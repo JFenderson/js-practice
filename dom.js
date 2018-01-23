ac967de8bd6c2ab8e5f32748362c5d85b5ff72fa
JFenderson <fenderson.joseph@gmail.com>
2018-01-23 13:42:46 -0600

    squares

--- dom.js
+++ dom.js
@@ -14,52 +14,55 @@ document.addEventListener("DOMContentLoaded",() => {
 
     btn.addEventListener("click",function(){
 
-    let container = document.createElement("div");
-    container.className = "sqContainer";
-    container.id = "sqId" + counter++;
-    applicationArea.appendChild(container); 
+        let container = document.createElement("div");
+        container.className = "sqContainer";
+        container.id = ++counter;
+        //var sqIndex = "sqId" + counter;
+        applicationArea.appendChild(container); 
               
-    let text = document.createElement("p");
-    text.className = "pContainer";
-    text.innerText = counter;
-    text.style.color = "transparent"
-    container.appendChild(text);
+        let text = document.createElement("p");
+        text.className = "pContainer";
+        text.innerText = counter;
+        text.style.color = "transparent"
+        container.appendChild(text);
 
-    container.addEventListener("click", function(){
-        var rgb = [];
-        for (var i = 0;i < 3;i++)
-            rgb.push(Math.floor(Math.random()* 255));
-            container.style.backgroundColor = 'rgb('+ rgb.join(',') +')';
-    })
-
-    container.addEventListener("mouseover", function(){
-        text.style.color = "black";
-        container.addEventListener("mouseleave", function(){
-            text.style.color = "transparent";
+        container.addEventListener("click", function(){
+            var rgb = [];
+            for (var i = 0;i < 3;i++)
+                rgb.push(Math.floor(Math.random()* 255));
+                container.style.backgroundColor = 'rgb('+ rgb.join(',') +')';
         })
-     })
 
+        container.addEventListener("mouseover", function(){
+            text.style.color = "black";
+                container.addEventListener("mouseleave", function(){
+                    text.style.color = "transparent";
+                })
+        })
 
+        console.log(counter)
 
-    container.addEventListener("dblclick", function removeElement(appArea,sqId){
-    var myDiv = applicationArea.getElementsByClassName("sqContainer");
-    var k;
-    for(k = 0;k < myDiv.length;k++){
-        if (myDiv[k] % 2 === 0){
-            alert("even:delete after square");
-        }else if (myDiv[k] % 3 === 0) {
-            alert("odd:delete previous square");
-            // var child = document.getElementById("sqId");
-            // var parent = document.getElementById(appArea);
-            // parent.removeChild(sqId);
-        }else{
-            alert("Child div has already been removed and does not exist");
-            return false;
-        }
-    }
+        container.addEventListener("dblclick", function (item){
+            
+            console.log(item);
+
+            if (item.target.id % 2 === 0){
+                console.log("Even");
+                console.log(item.target.id);
+                //console.log(document.getElementBy(item.target.id).nextSibling);
+                document.getElementById(item.target.id).nextSibling.remove();
+            }else if (item.target.id % 2 !== 0) {
+                console.log("odd");
+                console.log(item.target.id);
+                console.log(document.getElementById(item.target.id).previousSibling);
+                document.getElementById(item.target.id).previousSibling.remove();
+              }
+              //if(sqIndex = undefined){
+            //      alert("no");
+            //  }
+            }) 
+
     })
-
-})
 });
     
 
